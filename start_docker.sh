b3f03c7e11e708ca2b63841f7be5521b271be0b2
vagrant <vagrant@vagrant.vm>
2021-05-21 19:38:28 +0000

    Add bash file for starting up docker container

---
+++ start_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+month=$(date +"%m")
+day=$(date +"%d")
+year=$(date +"%Y")
+
+today="./../tmp/data/${year}/${month}/${day}"
+
+if [ -d "./../tmp/data/${year}/${month}/${day}" ]
+then
+    echo "Directory /path/to/dir exists."
+else
+    echo "Creating Directory"
+    mkdir "${today}"
+fi
+
+cd ./../sirepo-bluesky
+docker run --rm -e SIREPO_AUTH_METHODS=bluesky:guest -e SIREPO_AUTH_BLUESKY_SECRET=bluesky -e SIREPO_SRDB_ROOT=/sirepo -e SIREPO_COOKIE_IS_SECURE=false -p 8000:8000 -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z radiasoft/sirepo:20200220.135917 bash -l -c "mkdir -v -p /sirepo/user/ && cp -Rv /SIREPO_SRDB_ROOT/* /sirepo/user/ && sirepo service http"
