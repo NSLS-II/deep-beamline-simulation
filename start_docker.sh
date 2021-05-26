232ae3130c5e51afde22fc79352c552c5adaf922
vagrant <vagrant@vagrant.vm>
2021-05-26 18:15:48 +0000

    move directories

--- start_docker.sh
+++ start_docker.sh
@@ -18,5 +18,6 @@ apt-get update
 apt-get install sirepo-bluesky -y
 git clone https://github.com/NSLS-II/sirepo-bluesky.git
 ls -l
+cd sirepo-bluesky/
 
 docker run --rm -e SIREPO_AUTH_METHODS=bluesky:guest -e SIREPO_AUTH_BLUESKY_SECRET=bluesky -e SIREPO_SRDB_ROOT=/sirepo -e SIREPO_COOKIE_IS_SECURE=false -p 8000:8000 -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z radiasoft/sirepo:20200220.135917 bash -l -c "mkdir -v -p /sirepo/user/ && cp -Rv /SIREPO_SRDB_ROOT/* /sirepo/user/ && sirepo service http"
