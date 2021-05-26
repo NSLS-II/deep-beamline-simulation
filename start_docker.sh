694984f7e401419e6d2c7c45a6b81daea764e479
vagrant <vagrant@vagrant.vm>
2021-05-26 15:38:03 +0000

    fix sirepo issues

--- start_docker.sh
+++ start_docker.sh
@@ -14,5 +14,8 @@ else
     mkdir "${today}"
 fi
 
+pip install sirepo-bluesky
+git clone https://github.com/NSLS-II/sirepo-bluesky.git
 cd ./../sirepo-bluesky
+
 docker run --rm -e SIREPO_AUTH_METHODS=bluesky:guest -e SIREPO_AUTH_BLUESKY_SECRET=bluesky -e SIREPO_SRDB_ROOT=/sirepo -e SIREPO_COOKIE_IS_SECURE=false -p 8000:8000 -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z radiasoft/sirepo:20200220.135917 bash -l -c "mkdir -v -p /sirepo/user/ && cp -Rv /SIREPO_SRDB_ROOT/* /sirepo/user/ && sirepo service http"
