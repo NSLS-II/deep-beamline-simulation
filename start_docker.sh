3e3ed392085b627ee5a7490681afafbde27c6042
jennmald <jennmaldona@gmail.com>
2021-08-03 17:24:19 +0000

    change docker container to match sirepo

--- start_docker.sh
+++ start_docker.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-month=$(date +"%m")
-day=$(date +"%d")
-year=$(date +"%Y")
+docker run --rm -p 8000:8000 -v "$PWD:/sirepo" radiasoft/sirepo
 
-today="${HOME}/tmp/data/${year}/${month}/${day}"
+#month=$(date +"%m")
+#day=$(date +"%d")
+#year=$(date +"%Y")
 
-if [ -d "${today}" ]
-then
-    echo "Directory ${today} exists."
-else
-    echo "Creating Directory ${today}"
-    mkdir -p "${today}"
-fi
+#today="${HOME}/tmp/data/${year}/${month}/${day}"
 
-mkdir -p $HOME/tmp/sirepo-docker-run
+#if [ -d "${today}" ]
+#then
+#    echo "Directory ${today} exists."
+#else
+#    echo "Creating Directory ${today}"
+#    mkdir -p "${today}"
+#fi
 
-ls -l $HOME/tmp
+#mkdir -p $HOME/tmp/sirepo-docker-run
+
+#ls -l $HOME/tmp
 
 # specify -it or -d on the command line
-docker run $1 --init --rm --name sirepo \
-       -e SIREPO_AUTH_METHODS=bluesky:guest \
-       -e SIREPO_AUTH_BLUESKY_SECRET=bluesky \
-       -e SIREPO_SRDB_ROOT=/sirepo \
-       -e SIREPO_COOKIE_IS_SECURE=false \
-       -p 8000:8000 \
-       -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z \
-       radiasoft/sirepo:beta bash -l -c "sirepo service http"
+#docker run $1 --init --rm --name sirepo \
+#       -e SIREPO_AUTH_METHODS=bluesky:guest \
+#       -e SIREPO_AUTH_BLUESKY_SECRET=bluesky \
+#       -e SIREPO_SRDB_ROOT=/sirepo \
+#       -e SIREPO_COOKIE_IS_SECURE=false \
+#       -p 8000:8000 \
+#       -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z \
+#       radiasoft/sirepo:beta bash -l -c "sirepo service http"
