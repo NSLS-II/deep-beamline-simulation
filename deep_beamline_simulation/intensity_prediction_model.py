b35a94955f80b97adc534621fd69604d8d903ea4
jennmald <jennmaldona@gmail.com>
2021-08-10 18:06:16 +0000

    updates to intensity model

--- deep_beamline_simulation/intensity_prediction_model.py
+++ deep_beamline_simulation/intensity_prediction_model.py
@@ -1,170 +1,176 @@
 import re
 import os
 import time
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torchvision
+import numpy as np
 from torch.nn import *
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch.autograd import Variable
-from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
+from torch.utils.tensorboard import SummaryWriter
 from sklearn.preprocessing import PolynomialFeatures
 
-
-# use for normalization
-def min_max(lists):
-    # large numbers to override in search process
-    minimum = 1e20
-    maximum = 1e-20
-    for l in lists:
-        for e in l:
-            if e < minimum:
-                minimum = e
-            if e > maximum:
-                maximum = e
-    return minimum, maximum
-
-
-# define neural network
 class Neural_Net(torch.nn.Module):
     def __init__(self):
-        super(Neural_Net, self).__init__()
-        # define layers
-        self.hidden1 = torch.nn.Linear(3, 256)
-        self.hidden2 = torch.nn.Linear(256, 512)
-        # define dropout
+        ''' Define Neural Network '''
+        super(Neural_Net,self).__init__()
+        self.input = torch.nn.Linear(3,256)
+        self.hidden1 = torch.nn.Linear(256, 512)
         self.dropout1 = torch.nn.Dropout(0.6)
-        self.hidden3 = torch.nn.Linear(512, 512)
+        self.hidden2 = torch.nn.Linear(512,512)
         self.dropout2 = torch.nn.Dropout(0.6)
-        self.hidden4 = torch.nn.Linear(512, 256)
+        self.hidden3 = torch.nn.Linear(512, 256)
         self.output = torch.nn.Linear(256, 70)
 
     def forward(self, x):
-        # call layers and use relu activation function
+        ''' Call layers and use ReLU activation function '''
+        x = F.relu(self.input(x))
         x = F.relu(self.hidden1(x))
-        x = F.relu(self.hidden2(x))
         x = self.dropout1(x)
-        x = F.relu(self.hidden3(x))
+        x = F.relu(self.hidden2(x))
         x = self.dropout2(x)
-        x = F.relu(self.hidden4(x))
+        x = F.relu(self.hidden3(x))
         x = self.output(x)
         return x
 
+    def parse_data(self, data):
+        ''' Fix data to be in neural network input format '''
+        pos_int = {}
+        for file, parameters in data.items():
+            position_intensity = np.genfromtxt(file, delimiter=',')
+            position_intensity = np.array(position_intensity[1:])
+            # remove headers
+            pos_int[file] = position_intensity
+        parsed = []
+        pos_ints_values = pos_int.values()
+        for lst in pos_ints_values:
+            ints = lst[:, 1]
+            parsed.append(np.array(ints))
+        return parsed
 
-# data organization - file names and parameters
-# to be automated
-data = {}
-data["210301_horpos.csv"] = [21.0, 0.3, 0.1]
-data["210102_horpos.csv"] = [21.0, 0.1, 0.2]
-data["210305_hospos.csv"] = [21.0, 0.3, 0.5]
-data["210304_horpos.csv"] = [21.0, 0.3, 0.4]
-data["210205_hospos.csv"] = [21.0, 0.2, 0.5]
-data["210201_horpos.csv"] = [21.0, 0.2, 0.1]
-data["210303_horpos.csv"] = [21.0, 0.3, 0.3]
-data["210105_horpos.csv"] = [21.0, 0.1, 0.5]
-data["210203_horpos.csv"] = [21.0, 0.2, 0.3]
-data["210204_hospos.csv"] = [21.0, 0.2, 0.4]
-data["210103_horpos.csv"] = [21.0, 0.1, 0.3]
-data["210104_horpos.csv"] = [21.0, 0.1, 0.4]
-data["210202_horpos.csv"] = [21.0, 0.2, 0.2]
-data["210101_horpos.csv"] = [21.0, 0.1, 0.1]
-data["210302_horpos.csv"] = [21.0, 0.3, 0.2]
+    def min_max(self, l):
+        ''' Use for normalizing data '''
+        minimum = 1e20
+        maximum = -1e20
+        for i in l:
+            if i < minimum:
+                minimum = i
+            if i > maximum:
+                maximum = i
+        return minimum, maximum
 
-# read data from file
-pos_int = {}
-path_to_file = "Data_Collection/"
-for file, parameters in data.items():
-    position_intensity = np.genfromtxt(path_to_file + file, delimiter=",")
-    # remove headers
-    position_intensity = np.array(position_intensity[1:])
-    pos_int[file] = position_intensity
-# strip file names and obtain all data
-trainx = list(data.values())
-trainy = []
-pos_ints_vals = pos_int.values()
-# turn into proper formatting for training
-for lst in pos_ints_vals:
-    ints = lst[:, 1]
-    trainy.append(np.array(ints))
+    def normalize_data(self, data):
+        ''' Normalize data to be [0,1] '''
+        norm_data = []
+        minimum, maximum = self.min_max(data)
+        for i in data:
+            norm_data.append((i-minimum)/ (maximum-minimum))
+        return norm_data
 
-min_y, max_y = min_max(trainy)
-norm_trainy = []
-for y in trainy:
-    # normalize train y
-    norm_trainy.append((y - min_y) / (max_y - min_y))
+    def accuracy(self, predicted, actual):
+        correct = 0
+        for i in range(0, len(predicted)):
+             if predicted[i]-actual[i] < 0.01:
+                 correct+=1
+        acc = (correct/len(predicted))*100
+        return acc
 
-# define writer for tensorboard implementation
-writer = SummaryWriter()
 
-# create model object
-model = Neural_Net()
-# display model
-print("Model Structure")
-print(model)
+def main():
+    writer =  SummaryWriter()
+    model = Neural_Net()
+    print('Model Structure')
+    print(model)
 
-# convert numpy array to tensor of input shape and convert to float type
-x = torch.from_numpy(np.array(trainx)).float()
-y = torch.from_numpy(np.array(norm_trainy)).float()
+    training_data = {}
+    #training_data["210301_horpos.csv"] = [21.0, 0.3, 0.1]
+    training_data['210051_horpos.csv'] = [21.0, 0.05, 1]
 
-# optimizer and loss
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-loss_func = torch.nn.MSELoss()
+    trainx = list(training_data.values())
+    temp_trainy = model.parse_data(training_data)
+    trainy = []
+    for lst in temp_trainy:
+        d = model.normalize_data(lst)
+        trainy.append(d)
 
-# change type to Variable (contains gradients)
-inputs = Variable(x)
-outputs = Variable(y)
+    testing_data = {}
+    testing_data['2100202_horpos.csv'] = [21.0, 0.02, 0.2]
+    testx = list(testing_data.values())
+    temp_testy = model.parse_data(testing_data)
+    testy = []
+    for lst in temp_testy:
+        d = model.normalize_data(lst)
+        testy.append(d)
 
-# define a loss list
-loss_plot = []
+    trainx = torch.from_numpy(np.array(trainx)).float()
+    trainy = torch.from_numpy(np.array(trainy)).float()
+    testx = torch.from_numpy(np.array(testx)).float()
+    testy = torch.from_numpy(np.array(testy)).float()
 
-# define data location for tensorboard access
-file_path = "/vagrant/tensorboard.txt"
-# open file
-file = open(file_path, "w")
+    optimizer = torch.optim.Adam(model.parameters(), lr =0.1)
+    loss_func = torch.nn.MSELoss()
 
-# start training time
-t_start = time.time()
+    train_inputs = Variable(trainx)
+    train_outputs =  Variable(trainy)
+    test_inputs = Variable(testx)
+    test_outputs =  Variable(testy)
 
-# begin training
-for i in range(5000):
-    prediction = model(inputs)
-    loss = loss_func(prediction, outputs)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    # collect loss
-    loss_plot.append(loss.data.numpy())
+    loss_plot = []
+    accuracy_plot = []
+    file_path1='/vagrant/loss.txt'
+    file1 = open(file_path1, 'w')
+    file_path2='/vagrant/accuracy.txt'
+    file2 = open(file_path2, 'w')
 
-    if i % 1000 == 0:
-        # plot and show learning process
-        plt.cla()
-        for i in range(len(trainy)):
+    t_start = time.time()
+    for e in range(5000):
+        prediction = model(train_inputs)
+        loss = loss_func(prediction, train_outputs)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        loss_plot.append(loss.data.numpy())
+        points = np.linspace(-200, 200, len(trainy[0]))
+        if e % 1000 == 0:
             plt.cla()
-            plt.plot(
-                np.linspace(-200, 200, len(trainy[0])), norm_trainy[i], label="original"
-            )
-            plt.title(i)
-            plt.plot(
-                np.linspace(-200, 200, len(norm_trainy[0])),
-                prediction.data.numpy()[i],
-                label="predicted",
-            )
-            plt.text(0, 0, "Loss=%.4f" % loss.data.numpy())
-            plt.legend()
-            plt.pause(0.5)
-            plt.plot()
+            for i in range(len(trainy)):
+                plt.cla()
+                plt.plot(points, trainy[i], label = 'original')
+                plt.title(e)
+                plt.plot(points, prediction.data.numpy()[i], label='predicted')
+                plt.text(-200, 1, 'Loss=%.4f' % loss.data.numpy())
+                plt.text(-200, 0.89, 'Accuracy=%.4f' % model.accuracy(prediction.data.numpy()[i], trainy.numpy()[i]))
+                plt.legend(loc = 'upper right')
+                plt.pause(1)
+        for i in range(len(trainy)):
+            accuracy_plot.append(model.accuracy(prediction.data.numpy()[i], trainy.numpy()[i]))
+    print('training completed')
+    t_end = time.time()
+    print('Time elapsed: ' + str((t_end-t_start)/60.0))
+    for element in loss_plot:
+        file1.write(str(element)+'\n')
+    for i in accuracy_plot:
+        file2.write(str(i)+'\n')
+    file1.close()
+    file2.close()
 
-# note training has ended
-print("training completed")
+    test_plot = []
+    with torch.no_grad():
+        model.eval()
+        model_pred = model(testx)
+        test_loss = loss_func(model_pred, testy)
+    test_plot = model_pred.data.numpy()[0]
+    plt.show()
+    plt.plot(points, test_plot, label='predicted')
+    plt.plot(points, testy[0], label='actual')
+    plt.text(-200,0.88, 'Loss=%.4f' % test_loss)
+    plt.text(-200,0.75, 'Accuracy=%.4f' % model.accuracy(test_plot, testy[0]))
+    plt.title('Model Predictions on Test Data')
+    plt.legend(loc = 'upper right')
+    plt.show()
 
-# end training time
-t_end = time.time()
+if __name__ == '__main__':
+    main()
 
-# print time in minutes elapsed
-print("Time elapsed: " + str((t_end - t_start) / 60.0))
-
-for element in loss_plot:
-    file.write(str(element) + "\n")
-file.close()
