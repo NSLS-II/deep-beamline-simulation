9524f4c0cf00fd3c879089b894096b6b56b59711
Joshua Lynch <jlynch@bnl.gov>
2021-08-26 12:18:05 -0400

    try using /tmp for SIREPO_SRDB_ROOT

--- scripts/start_sirepo.sh
+++ scripts/start_sirepo.sh
@@ -36,7 +36,7 @@ in_docker_cmd="mkdir -p /sirepo/sirepo-db && sirepo service http"
 cmd="docker run $1 --init --rm --name sirepo \
        -e SIREPO_AUTH_METHODS=bluesky:guest \
        -e SIREPO_AUTH_BLUESKY_SECRET=bluesky \
-       -e SIREPO_SRDB_ROOT=/sirepo/sirepo-db \
+       -e SIREPO_SRDB_ROOT=/tmp \
        -e SIREPO_COOKIE_IS_SECURE=false \
        -p 8000:8000 \
        -v $HOME/tmp/sirepo-docker-run:/sirepo \
