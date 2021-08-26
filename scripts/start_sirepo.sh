c33bc966174d1fb96a353a213b36d382ae4519fd
Joshua Lynch <jlynch@bnl.gov>
2021-08-26 11:56:47 -0400

    let the sirepo docker container create the sirepo db directory in hopes of fixing CI

--- scripts/start_sirepo.sh
+++ scripts/start_sirepo.sh
@@ -25,18 +25,18 @@ else
     mkdir -p "${today}"
 fi
 
-# sirepo needs this directory
+# sirepo will use this directory
 mkdir -p ${HOME}/tmp/sirepo-docker-run
 
 docker_image="radiasoft/sirepo:beta"
 docker pull ${docker_image}
 docker images
 
-in_docker_cmd="sirepo service http"
+in_docker_cmd="mkdir -p /sirepo/sirepo-db && sirepo service http"
 cmd="docker run $1 --init --rm --name sirepo \
        -e SIREPO_AUTH_METHODS=bluesky:guest \
        -e SIREPO_AUTH_BLUESKY_SECRET=bluesky \
-       -e SIREPO_SRDB_ROOT=/sirepo \
+       -e SIREPO_SRDB_ROOT=/sirepo/sirepo-db \
        -e SIREPO_COOKIE_IS_SECURE=false \
        -p 8000:8000 \
        -v $HOME/tmp/sirepo-docker-run:/sirepo \
