7f062b753e62ee7e129c1b90c30960ecc217a11f
Jennefer Maldonado <jennmaldona@gmail.com>
2021-11-18 21:41:11 +0000

    u_net documentation and image processing

--- deep_beamline_simulation/images.py
+++ deep_beamline_simulation/images.py
@@ -11,39 +11,30 @@ from torchinfo import summary
 from torch.utils.data import DataLoader
 
 
-# file dictionary for importing
-'''
-im_data = (
-    Path(deep_beamline_simulation.__path__[0]).parent
-    / "deep_beamline_simulation/image_data"
-)
-
-images = []
-for filename in os.listdir(im_data):
-    im_data_path = im_data / filename
-    images.append(im_data_path)
-
-fig, axes = plt.subplots(1, 8)
-fig.suptitle("Varying Radius of Surface Curvature", fontsize=20)
-
-image_data = []
-for i in range(0, len(images)):
-    image = pandas.read_csv(images[i], skiprows=1)
-    image_data.append( (torch.tensor(image.to_numpy()), torch.from_numpy(np.random.rand(1, 20, 20).astype("f"))) )
-    axes[i].imshow(image, aspect="auto")
-#plt.show()
-
-image_dataset = DataLoader(image_data, batch_size=1, shuffle=True)
-#print(image_data)
-'''
 train_file = 'image_data/Initial-Intensity-33-1798m.csv'
 output_file = 'image_data/Intensity-At-Sample-63-3m.csv'
-train_image = torch.tensor( pandas.read_csv(train_file, skiprows=1).to_numpy() )
-output_image = torch.tensor( pandas.read_csv(output_file, skiprows=1).to_numpy() )
+# torch.from_numpy(np.random.rand(3, 20, 20).astype("f"))
+
+train_numpy = pandas.read_csv(train_file, skiprows=1).to_numpy()
+output_numpy = pandas.read_csv(output_file, skiprows=1).to_numpy()
+
+train_image = torch.from_numpy(train_numpy.astype('f'))
+output_image = torch.from_numpy(output_numpy.astype('f'))
+
+print('Train Image Size')
+print(train_image.size())
+print('Output Image Size')
+print(output_image.size())
 
 model = UNet()
-#print(model)
-#summary(model)
+
+train_image = train_image[None, None, :]
+#output_image = output_image[None, None, :]
+print('Train Image Size After Adding Channels')
+print(train_image.shape)
+#print('Output Image Size After Adding Channels')
+#print(output_image.shape)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 loss_func = torch.nn.MSELoss()
 
@@ -53,10 +44,14 @@ training_loss = 0.0
 
 for e in range(0, 10):
     predictions = model(train_image)
+    print(predictions.shape)
+    # print(outputs.shape)
     loss = loss_func(predictions, output_image)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+
 print("Test Loss: " + str(loss))
 test_predictions = None
 with torch.no_grad():
@@ -66,9 +61,3 @@ with torch.no_grad():
     test_plot = test_predictions.data.numpy()[0]
 print("Test Loss: " + str(test_loss))
 print(test_predictions)
-
-
-
-
-
-
--- deep_beamline_simulation/u_net.py
+++ deep_beamline_simulation/u_net.py
@@ -1,4 +1,3 @@
-# import statements
 import torch
 import numpy as np
 import torchvision
@@ -8,83 +7,108 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
 
-""" Create the basic block architecture"""
-
-
 class Block(nn.Module):
-    def __init__(self, input_channels=3, output_channels=3):
+    """ 
+    Create the basic block architecture with conv2d in and out and RELU activation
+    Assumes kernel size is 3, stride is 1, and padding is 1
+    
+    Parameters
+    ----------
+    input_channels : int
+        The number of channels in the input image
+        Set to 1 if no channels necessary
+    output_channels : int
+        The number of channels in the output image
+        Set to 1 if no channels necessary
+    """
+
+    def __init__(self, input_channels=1, output_channels=3):
         super().__init__()
-        # kernel_size = 3
-        self.conv_in = nn.Conv2d(
+        self.input_layer = nn.Conv2d(
             input_channels, output_channels, 3, stride=1, padding=1
         )
         self.relu = nn.ReLU()
-        # kernel_size = 3
-        self.conv_out = nn.Conv2d(
+        self.output_layer = nn.Conv2d(
             output_channels, output_channels, 3, stride=1, padding=1
         )
 
     def forward(self, x):
-        x = self.conv_in(x)
+        x = self.input_layer(x)
         x = self.relu(x)
-        x = self.conv_out(x)
-        return self.relu(x)
-
-
-""" similar to standard CNN """
+        x = self.output_layer(x)
+        x = self.relu(x)
+        return x
 
 
 class Encoder(nn.Module):
-    def __init__(self, num_channels=(3, 64, 128)):
+    """ 
+    Creates the Encoder block using the Block class that contracts images
+    The encoder is similar to a standard (Convolutional Neural Network CNN)
+    Assumes kernel size is 3, stride is 1, and padding is 1 from class Block
+    
+    Parameters
+    ----------
+    num_channels : list, tuple
+        As the image trains the number of channels will be increased
+        Later to be decreased by the conv transpose
+    """
+
+    def __init__(self, num_channels=(1, 64, 128)):
         super().__init__()
-        # creates a block for each pair of channels
-        self.enc_blocks = nn.ModuleList(
-            [
-                Block(num_channels[i], num_channels[i + 1])
-                for i in range(len(num_channels) - 1)
-            ]
-        )
-        self.pool = nn.MaxPool2d(2)
+        block_list = []
+        for i in range(len(num_channels) - 1):
+            block_list.append(Block(num_channels[i], num_channels[i + 1]))
+
+        self.encoder_blocks = nn.ModuleList(block_list)
+        self.maxpool = nn.MaxPool2d(2)
 
     def forward(self, x):
-        out = []
-        for block in self.enc_blocks:
+        output = []
+        for block in self.encoder_blocks:
             x = block(x)
-            out.append(x)
-            x = self.pool(x)
-        return out
+            output.append(x)
+            x = self.maxpool(x)
+        return output
 
 
 class Decoder(nn.Module):
-    """ up convolution part """
+    """ 
+    Creates the Decoder block using the Block class to expand images
+    Up samples image at each step to increase image from low to high resolution
+    Assumes kernel size is 3, stride is 1, and padding is 1 from class Block
+    
+    Parameters
+    ----------
+    num_channels : list, tuple
+        As the image trains the number of channels will be decreased from 
+        the maximum size from the encoder
+
+    Methods
+    -------
+    crop - Downsizes x to ensure correct output shape
+    """
 
     def __init__(self, num_channels=(128, 64)):
         super().__init__()
         self.num_channels = num_channels
         # conv transpose for decreasing pairs of channels
-        self.upconvs = nn.ModuleList(
-            [
+        upconv_list = []
+        decblock_list = []
+        for i in range(len(num_channels) - 1):
+            upconv_list.append(
                 nn.ConvTranspose2d(num_channels[i], num_channels[i + 1], 2, 2)
-                for i in range(len(num_channels) - 1)
-            ]
-        )
-        self.dec_blocks = nn.ModuleList(
-            [
-                Block(num_channels[i], num_channels[i + 1])
-                for i in range(len(num_channels) - 1)
-            ]
-        )
+            )
+            decblock_list.append(Block(num_channels[i], num_channels[i + 1]))
+        self.upconv_modules = nn.ModuleList(upconv_list)
+        self.decoder_blocks = nn.ModuleList(decblock_list)
 
     def forward(self, x, encoder_features):
         for i in range(len(self.num_channels) - 1):
-            x = self.upconvs[i](x)
+            x = self.upconv_modules[i](x)
             encoder_features = self.crop(encoder_features[i], x)
-            # concat the unconvs and encoder features
             x = torch.cat([x, encoder_features], dim=1)
-            x = self.dec_blocks[i](x)
+            x = self.decoder_blocks[i](x)
         return x
-
-    """ Downsizing and creating the correct output shape """
 
     def crop(self, encoder_features, x):
         _, _, H, W = x.shape
@@ -93,25 +117,29 @@ class Decoder(nn.Module):
 
 
 class UNet(nn.Module):
+    """ 
+    Creates a Unet model using the Block, Encoder, and Decoder classes
+    
+    Parameters
+    ----------
+    encoder_channels : list, tuple
+        same channels as the encoder class
+    decoder_channels : list, tuple
+        same channels as the decoder class
+    groups : int
+        default is 1 so that all inputs are convolved to all outputs
+    """
+
     def __init__(
-        self,
-        encoder_channels=(3, 64, 128),
-        decoder_channels=(128, 64),
-        num_class=1,
-        retain_dim=False,
-        out_sz=(572, 572),
+        self, encoder_channels=(1, 64, 128), decoder_channels=(128, 64), groups=1,
     ):
         super().__init__()
         self.encoder = Encoder(encoder_channels)
         self.decoder = Decoder(decoder_channels)
-        self.head = nn.Conv2d(decoder_channels[-1], num_class, 1)
-        self.retain_dim = retain_dim
+        self.head = nn.Conv2d(decoder_channels[-1], groups, 1)
 
     def forward(self, x):
         encoder_features = self.encoder(x)
         out = self.decoder(encoder_features[::-1][0], encoder_features[::-1][1:])
         out = self.head(out)
-        if self.retain_dim:
-            # interpolates output if the current output is not the correct output size
-            out = F.interpolate(out, out_sz)
         return out
