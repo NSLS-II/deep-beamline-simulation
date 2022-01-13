86e9940579b0fee988b713bc4da418a3f4757a8f
Jennefer Maldonado <jennmaldona@gmail.com>
2022-01-13 17:44:34 +0000

    add maxpool, fix test issues, customize input and output shapes

--- deep_beamline_simulation/tests/test_nn.py
+++ deep_beamline_simulation/tests/test_nn.py
@@ -6,55 +6,59 @@ from pathlib import Path
 import deep_beamline_simulation
 from deep_beamline_simulation.u_net import UNet, ImageProcessing
 
+
 def test_predictions():
-	'''
-	Simple test function to ensure the model returns an 
-	output image when given and input image
-	'''
-	dbs_dir = (Path(deep_beamline_simulation.__path__[0]).parent / 'deep_beamline_simulation/image_data')
-	train_file = dbs_dir / 'Initial-Intensity-33-1798m.csv'
-	output_file = dbs_dir / "Intensity-At-Sample-63-3m.csv"
+    """
+    Simple test function to ensure the model returns an
+    output image when given and input image
+    """
+    dbs_dir = (
+        Path(deep_beamline_simulation.__path__[0]).parent
+        / "deep_beamline_simulation/image_data"
+    )
+    train_file = dbs_dir / "Initial-Intensity-33-1798m.csv"
+    output_file = dbs_dir / "Intensity-At-Sample-63-3m.csv"
 
-	# read csv using pandas, skip first row for headers
-	train_numpy = pandas.read_csv(train_file, skiprows=1).to_numpy()
-	output_numpy = pandas.read_csv(output_file, skiprows=1).to_numpy()
+    # read csv using pandas, skip first row for headers
+    train_numpy = pandas.read_csv(train_file, skiprows=1).to_numpy()
+    output_numpy = pandas.read_csv(output_file, skiprows=1).to_numpy()
 
-	# create a list to store in ImageProcessing class
-	image_list = [train_numpy, output_numpy]
-	ip = ImageProcessing(image_list)
+    # create a list to store in ImageProcessing class
+    image_list = [train_numpy, output_numpy]
+    ip = ImageProcessing(image_list)
 
-	# find the size of the smallest image
-	height, length = ip.smallest_image_size()
+    # find the size of the smallest image
+    height, length = ip.smallest_image_size()
 
-	# resize all images bigger than the smallest image size
-	train_numpy = ip.resize(train_numpy, height, length)
-	resized_output_image = ip.resize(output_numpy, height, length)
+    # resize all images bigger than the smallest image size
+    train_numpy = ip.resize(train_numpy, height, length)
+    resized_output_image = ip.resize(output_numpy, height, length)
 
-	# normalize all training data
-	train_numpy = ip.normalize_image(train_numpy)
-	resized_output_image = ip.normalize_image(resized_output_image)
+    # normalize all training data
+    train_numpy = ip.normalize_image(train_numpy)
+    resized_output_image = ip.normalize_image(resized_output_image)
 
-	# convert each numpy array into a tensor
-	train_image = torch.from_numpy(train_numpy.astype("f"))
-	output_image = torch.from_numpy(resized_output_image.astype("f"))
+    # convert each numpy array into a tensor
+    train_image = torch.from_numpy(train_numpy.astype("f"))
+    output_image = torch.from_numpy(resized_output_image.astype("f"))
 
-	# create model
-	model = UNet()
+    # create model
+    model = UNet(140, 40)
 
-	train_image = train_image[None, None, :, :]
-	output_image = output_image[None, None, :, :]
+    train_image = train_image[None, None, :, :]
+    output_image = output_image[None, None, :, :]
 
-	# define optimizer and loss function
-	optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-	loss_func = torch.nn.MSELoss()
+    # define optimizer and loss function
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    loss_func = torch.nn.MSELoss()
 
-	predictions = []
-	# loop through many epochs
-	for e in range(0, 3):
-	    predictions = model(train_image)
-	    loss = loss_func(predictions, output_image)
-	    optimizer.zero_grad()
-	    loss.backward()
-	    optimizer.step()
+    predictions = []
+    # loop through many epochs
+    for e in range(0, 3):
+        predictions = model(train_image)
+        loss = loss_func(predictions, output_image)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-	assert predictions.size != 0
+    assert predictions.size != 0
--- deep_beamline_simulation/u_net.py
+++ deep_beamline_simulation/u_net.py
@@ -6,7 +6,16 @@ from PIL import Image
 import torch
 from torchinfo import summary
 from torch.utils.data import DataLoader
-from torch.nn import Upsample, ConvTranspose2d, Conv2d, MaxPool2d, Module, ModuleList, ReLU, Dropout
+from torch.nn import (
+    Upsample,
+    ConvTranspose2d,
+    Conv2d,
+    MaxPool2d,
+    Module,
+    ModuleList,
+    ReLU,
+    Dropout,
+)
 from torchvision.transforms import CenterCrop
 
 
@@ -61,41 +70,24 @@ class ImageProcessing:
 
 
 class UNet(Module):
-    def __init__(self, input_size=136, output_size=40):
+    def __init__(self, input_size, output_size):
         super().__init__()
         self.input_layer = Conv2d(1, 1, kernel_size=3, stride=1, padding=1)
         # for going down the U
         self.conv_inx64 = Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
         self.conv_64x128 = Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_128x256 = Conv2d(128, 256, kernel_size=3, stride = 1, padding=1)
+        self.conv_128x256 = Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
         self.conv_256x512 = Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
         self.conv_512x1024 = Conv2d(512, 1024, kernel_size=3, stride=1, padding=1)
 
-        self.relu_1 = ReLU()
-        self.relu_2 = ReLU()
-        self.relu_3 = ReLU()
-        self.relu_4 = ReLU()
-        self.relu_5 = ReLU()
-        self.relu_6 = ReLU()
-        self.relu_7 = ReLU()
-        self.relu_8 = ReLU()
-        self.relu_9 = ReLU()
+        self.relu = ReLU()
 
-        self.dropout_1 = Dropout(0.5)
-        self.dropout_2 = Dropout(0.5)
-        self.dropout_3 = Dropout(0.5)
-        self.dropout_4 = Dropout(0.5)
-        self.dropout_5 = Dropout(0.5)
-        self.dropout_6 = Dropout(0.5)
-        self.dropout_7 = Dropout(0.5)
-        self.dropout_8 = Dropout(0.5)
-        self.dropout_9 = Dropout(0.5)
+        self.dropout = Dropout(0.5)
 
-        self.maxpool_1 = MaxPool2d(2)
-        self.maxpool_2 = MaxPool2d(2)
-        self.maxpool_3 = MaxPool2d(2)
-        self.maxpool_4 = MaxPool2d(2)
-        self.maxpool_5 = MaxPool2d(2)
+        self.maxpool = MaxPool2d(2)
+
+        self.upsample = Upsample(scale_factor=2, mode="nearest")
+        self.upsample_final = Upsample(size=(input_size, output_size))
 
         # for going up the U
         self.upconv1024 = ConvTranspose2d(1024, 512, kernel_size=3, stride=1, padding=1)
@@ -108,21 +100,19 @@ class UNet(Module):
         self.conv_256x128 = Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
         self.conv_128x64 = Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
 
-
-        # used for blocks 
+        # used for down blocks
         self.conv64 = Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv128 = Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv256 = Conv2d(256, 256, kernel_size=3, stride = 1, padding=1)
+        self.conv256 = Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.conv512 = Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
         self.conv1024 = Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1)
 
-        # used for blocks 
+        # used for up blocks
         self.conv64_1 = Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv128_1 = Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv256_1 = Conv2d(256, 256, kernel_size=3, stride = 1, padding=1)
+        self.conv256_1 = Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.conv512_1 = Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
         self.conv1024_1 = Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1)
-
 
         self.output_layer = Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
 
@@ -131,72 +121,75 @@ class UNet(Module):
         # encoder block 1
         x = self.input_layer(inputs)
         x = self.conv_inx64(x)
-        x = self.dropout_1(x)
-        x = self.relu_1(x)
+        x = self.dropout(x)
+        x = self.relu(x)
         x = self.conv64(x)
-        #x = self.maxpool_1(x)
+        x = self.maxpool(x)
 
         # encoder block 2
         x = self.conv_64x128(x)
-        x = self.dropout_2(x)
-        x = self.relu_2(x)
+        x = self.dropout(x)
+        x = self.relu(x)
         x = self.conv128(x)
-        #x = self.maxpool_2(x)
+        x = self.maxpool(x)
 
         # encoder block 3
         x = self.conv_128x256(x)
-        x = self.dropout_3(x)
-        x = self.relu_3(x)
+        x = self.dropout(x)
+        x = self.relu(x)
         x = self.conv256(x)
-        #x = self.maxpool_3(x)
+        x = self.maxpool(x)
 
         # encoder block 4
         x = self.conv_256x512(x)
-        x = self.dropout_4(x)
-        x = self.relu_4(x)
+        x = self.dropout(x)
+        x = self.relu(x)
         x = self.conv512(x)
-        #x = self.maxpool_4(x)
+        # x = self.maxpool(x)
 
         # # encoder block 5
-        # x = self.conv_512x1024(x)
-        # x = self.dropout_5(x)
-        # x = self.relu_5(x)
-        # x = self.conv1024(x)
-        # x = self.maxpool_5(x)
+        x = self.conv_512x1024(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.conv1024(x)
+        # x = self.maxpool(x)
 
         # # up
         # # decoder block 1
-        # x = self.upconv1024(x)
-        # #x = self.conv_1024x512(x)
-        # x = self.dropout_6(x)
-        # x = self.relu_6(x)
-        # x = self.conv512_1(x)
+        x = self.upconv1024(x)
+        # x = self.conv_1024x512(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.conv512_1(x)
 
         # decoder block 2
         x = self.upconv512(x)
-        #x = self.conv_512x256(x)
-        x = self.dropout_7(x)
-        x = self.relu_7(x)
+        # x = self.conv_512x256(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.upsample(x)
         x = self.conv256_1(x)
 
         # decoder block 3
         x = self.upconv256(x)
-        #x = self.conv_256x128(x)
-        x = self.dropout_8(x)
-        x = self.relu_8(x)
+        # x = self.conv_256x128(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.upsample(x)
         x = self.conv128_1(x)
 
         # decoder block 4
         x = self.upconv128(x)
-        #x = self.conv_128x64(x)
-        x = self.dropout_9(x)
-        x = self.relu_9(x)
+        # x = self.conv_128x64(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.upsample_final(x)
         x = self.conv64_1(x)
 
         output = self.output_layer(x)
         return output
 
-        '''
+        """
         Going down the U
         conv 2d 64 
         relu, dropout, maxpool (to reduce dims)
@@ -226,9 +219,4 @@ class UNet(Module):
         relu, dropout
 
         output layer conv2d (64 ...)
-        '''
-
-
-
-
-
+        """
